2082e551233cd509d496d263f09381fcced47c72
Rofhiwa Matumba <matumba@genesis.home>
2025-01-24 01:02:37 +0200

    Outputted css files

--- utils/build-sass.sh
+++ utils/build-sass.sh
@@ -1,4 +1,4 @@
-sass styles/main.scss styles/out/main.css
-sass styles/browser.scss styles/out/browser.css
-sass styles/menu.scss styles/out/menu.css
-sass styles/layers.scss styles/out/layers.css
+sass ../styles/main.scss ../styles/main.css
+sass ../styles/browser.scss ../styles/browser.css
+sass ../styles/menu.scss ../styles/menu.css
+sass ../styles/layers.scss ../styles/layers.css
